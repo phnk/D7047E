ea1bf9f8f527247dd586357bac0c57ba8c9db55a
koninik <konni92@hotmail.com>
2020-05-18 10:56:24 +0200

    exercise 5

---
+++ Exercise 5/adversarial_images.py
@@ -0,0 +1,237 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# Take a look the training data
+print('Training.images shape: ', mnist.train.images.shape)
+print('Training.labels shape: ', mnist.train.labels.shape)
+print('Shape of an image: ', mnist.train.images[0].shape)
+print('Example label: ', mnist.train.labels[0])
+
+
+# Review a few images
+image_list = mnist.train.images[0:9]
+image_list_labels = mnist.train.labels[0:9]
+
+# https://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html#imagegrid
+fig = plt.figure(1, (5., 5.))
+grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                 nrows_ncols=(3, 3),  # creates 2x2 grid of axes
+                 axes_pad=0.3,  # pad between axes in inch.
+                 )
+
+for i in range(len(image_list)):
+    image = image_list[i].reshape(28,28)
+    grid[i].imshow(image)
+    grid[i].set_title('Label: {0}'.format(image_list_labels[i].argmax()))
+
+plt.show()
+
+# To run nicely in jupyter notebook
+sess = tf.InteractiveSession()
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+# Functions for convolution and pooling functions
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+
+def max_pooling_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+
+# Create placeholders nodes for images and label inputs
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# y = (Wx +b)
+# https://www.tensorflow.org/get_started/mnist/pros
+
+# Input layer
+x_image = tf.reshape(x, [-1,28,28,1]) # mnist image comes in as 784 vector
+
+# Conv layer 1 - 32x5x5
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+x_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+x_pool1 = max_pooling_2x2(x_conv1)
+
+# Conv layer 2 - 64x5x5
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+x_conv2 = tf.nn.relu(conv2d(x_pool1, W_conv2) + b_conv2)
+x_pool2 = max_pooling_2x2(x_conv2)
+
+# Flatten - keras 'flatten'
+x_flat = tf.reshape(x_pool2, [-1, 7*7*64])
+
+# Dense fully connected layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024]) # max pooling reduced image to 7x7
+b_fc1 = bias_variable([1024])
+x_fc1 = tf.nn.relu(tf.matmul(x_flat, W_fc1) + b_fc1)
+
+# Regularization with dropout
+keep_prob = tf.placeholder(tf.float32)
+x_fc1_drop = tf.nn.dropout(x_fc1, keep_prob)
+
+# Classification layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.matmul(x_fc1_drop, W_fc2) + b_fc2
+
+# Probabilities - output from model (not the same as logits)
+y = tf.nn.softmax(y_conv)
+
+# Loss and optimizer
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+# Setup to test accuracy of model
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Initilize all global variables
+sess.run(tf.global_variables_initializer())
+
+# Train model
+# Run once to get the model to a good confidence level
+for i in range(1000):
+    batch = mnist.train.next_batch(100)
+    if i%200 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.4})
+    
+    
+# Run trained model against test data
+print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images[0:500], 
+                                                  y_: mnist.test.labels[0:500], keep_prob: 1.0}))
+    
+
+def plot_predictions(image_list, output_probs=False, adversarial=False):
+    '''
+    Evaluate images against trained model and plot images.
+    If adversarial == True, replace middle image title appropriately
+    Return probability list if output_probs == True
+    '''
+    prob = y.eval(feed_dict={x: image_list, keep_prob: 1.0})
+    
+    pred_list = np.zeros(len(image_list)).astype(int)
+    pct_list = np.zeros(len(image_list)).astype(int)
+    
+    # Setup image grid
+    import math
+    cols = 3
+    rows = math.ceil(image_list.shape[0]/cols)
+    fig = plt.figure(1, (12., 12.))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                     nrows_ncols=(rows, cols),  # creates grid of axes
+                     axes_pad=0.5,  # pad between axes in inch.
+                     )
+    
+    # Get probs, images and populate grid
+    for i in range(len(prob)):
+        pred_list[i] = np.argmax(prob[i]) # for mnist index == classification
+        pct_list[i] = prob[i][pred_list[i]] * 100
+
+        image = image_list[i].reshape(28,28)
+        grid[i].imshow(image)
+        
+        grid[i].set_title('Label: {0} \nCertainty: {1}%' \
+                          .format(pred_list[i], 
+                                  pct_list[i]))
+        
+        # Only use when plotting original, partial deriv and adversarial images
+        if (adversarial) & (i % 3 == 1): 
+            grid[i].set_title("Adversarial \nPartial Derivatives")
+        
+    plt.show()
+    
+    return prob if output_probs else None
+
+# Get 10 4s [:,4] from top 500 [0:500], nonzero returns tuple, get index[0], then first 10 [0:10]
+index_of_4s = np.nonzero(mnist.test.labels[0:500][:,4])[0][0:10]
+x_batch = mnist.test.images[index_of_4s]
+
+plot_predictions(x_batch)
+
+# Get 10 9s [:,9] from top 500 [0:500], nonzero returns tuple, get index[0], then first 10 [0:10]
+#index_of_9s = np.nonzero(mnist.test.labels[0:500][:,2])[0][0:10]
+#x_batch = mnist.test.images[index_of_9s]
+
+#plot_predictions(x_batch)
+
+'''
+ADVERSARIAL IMAGES
+
+Create adversarial image of a 2 to be misclassified as a 6
+Create adversarial image of a 4 to be misclassified as a 9
+'''
+
+
+# Mostly inspired by:
+# https://codewords.recurse.com/issues/five/why-do-neural-networks-think-a-panda-is-a-vulture
+def create_plot_adversarial_images(x_image, y_label, lr=0.1, n_steps=1, output_probs=False):
+    
+    original_image = x_image
+    probs_per_step = []
+    
+    # Calculate loss, derivative and create adversarial image
+    # https://www.tensorflow.org/versions/r0.11/api_docs/python/train/gradient_computation
+    loss =  tf.nn.softmax_cross_entropy_with_logits(labels=y_label, logits=y_conv)
+    deriv = tf.gradients(loss, x)
+    image_adv = tf.stop_gradient(x - tf.sign(deriv)*lr/n_steps)
+    image_adv = tf.clip_by_value(image_adv, 0, 1) # prevents -ve values creating 'real' image
+    
+    for _ in range(n_steps):
+        # Calculate derivative and adversarial image
+        dydx = sess.run(deriv, {x: x_image, keep_prob: 1.0}) # can't seem to access 'deriv' w/o running this
+        x_adv = sess.run(image_adv, {x: x_image, keep_prob: 1.0})
+        
+        # Create darray of 3 images - orig, noise/delta, adversarial
+        x_image = np.reshape(x_adv, (1, 784))
+        img_adv_list = original_image
+        img_adv_list = np.append(img_adv_list, dydx[0], axis=0)
+        img_adv_list = np.append(img_adv_list, x_image, axis=0)
+
+        # Print/plot images and return probabilities
+        probs = plot_predictions(img_adv_list, output_probs=output_probs, adversarial=True)
+        probs_per_step.append(probs) if output_probs else None
+    
+    return probs_per_step
+
+
+# Pick a random 4 image from first 1000 images 
+# Create adversarial image and with target label 9
+index_of_4s = np.nonzero(mnist.test.labels[0:1000][:,4])[0]
+rand_index = np.random.randint(0, len(index_of_4s))
+image_norm = mnist.test.images[index_of_4s[rand_index]]
+image_norm = np.reshape(image_norm, (1, 784))
+label_adv = [0,0,0,0,0,0,0,0,0,1] # one hot encoded, adversarial label 9
+
+# Plot adversarial images
+# Over each step, model certainty changes from 4 to 9
+create_plot_adversarial_images(image_norm, label_adv, lr=0.2, n_steps=5)
+
+
+# Use a random noise image to classify it as 9
+
+
+#Use a zero image (matrix with all values 0) and classify it as 9.
+
+
+
+
+
+sess.close()
